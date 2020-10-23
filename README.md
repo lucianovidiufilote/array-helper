ed33f98f36e87e4c6460763df74e7bb4cfdb0e01
Lucian Filote <lucian.filote@gmail.com>
2020-10-24 01:08:39 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# array-helper
+array-helper
+=======
+
+A PHP array helper that does different usual and unusual operations on arrays
+
+Installation
+------------
+
+```bash
+composer require lucianovidiufilote/array-helper
+```
+
+Usage
+-----
+
+```php
+TBD
+```
---
+++ src/ArrayHelper.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lucian
+ * Date: 09.05.2018
+ * Time: 16:07
+ */
+
+namespace LucianOvidiuFilote\ArrayHelper;
+
+
+/**
+ * Class ArrayHelper
+ * @package AppBundle\Helpers
+ */
+class ArrayHelper
+{
+    public static function merge()
+    {
+
+    }
+}
