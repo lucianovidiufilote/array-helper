fd88b6b79abab6bc9edcd4a375cdc82041d8320f
Lucian Filote <lucian.filote@gmail.com>
2020-11-17 10:35:16 +0200

    fillArrayToLength getKeyOfMinValue getKeyOfMaxValue

--- src/ArrayHelper.php
+++ src/ArrayHelper.php
@@ -15,8 +15,25 @@ namespace LucianOvidiuFilote\ArrayHelper;
  */
 class ArrayHelper
 {
-    public static function merge()
+    public static function fillArrayToLength($array, $length, $value = null)
     {
-
+        end($array);
+        $lastIndex = key($array);
+        $startIndex = $lastIndex + 1;
+        $num = $length - count($array);
+        $fill = array_fill($startIndex, $num, $value);
+        return $array + $fill;
     }
+
+
+    public static function getKeyOfMinValue($array)
+    {
+        return array_keys($array, min($array))[0];
+    }
+
+    public static function getKeyOfMaxValue($array)
+    {
+        return array_keys($array, max($array))[0];
+    }
+
 }
